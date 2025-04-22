91992d0daeaa1f3c404828f2309a159ebd41674c
jejemerong <9708jjw@gmail.com>
2025-04-22 12:23:03 +0900

    feat: useCoupon, useProduct 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,19 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct: (updatedProduct: Product) => {
+      setProducts((prevProducts) =>
+        prevProducts.map((product) =>
+          product.id === updatedProduct.id ? updatedProduct : product
+        )
+      );
+    },
+    addProduct: (newProduct: Product) => {
+      setProducts((prevProducts) => [...prevProducts, newProduct]);
+    },
   };
 };
