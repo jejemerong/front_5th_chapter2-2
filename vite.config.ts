bf2cbaf9323afaa23e17bebb7f0db154c1b3086a
jejemerong <9708jjw@gmail.com>
2025-04-25 05:29:01 +0900

    deploy: 배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter2-2/" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          origin: resolve(__dirname, "index.origin.html"),
+          refactoring: resolve(__dirname, "index.refactoring.html"),
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
